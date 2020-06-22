05074da37966feff483f76dc443bcd2263faddec
Mark Whatcott <whatcott1@gmail.com>
2020-06-21 20:58:14 -0500

    updates

--- main2.py
+++ main2.py
@@ -5,26 +5,6 @@ import os
 from youtube_dl import YoutubeDL
 
 
-class MyLogger(object):
-    def debug(self, msg):
-        pass
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        print(msg)
-
-
-def postdownload(d):
-    if d["status"] == "finished":
-        print("Done downloading  ...")
-        console.quicklook(d["filename"])
-        os.remove(d["filename"])
-        appex.finish()
-        return
-
-
 outdir = os.path.expanduser("~/Documents/Downloads")
 try:
     os.mkdir(outdir)
@@ -34,29 +14,25 @@ except FileExistsError:
 if len(sys.argv) != 2:
     raise IndexError("usage: %s url" % sys.argv[0])
 url = sys.argv[1]
-print(f"url: {url}")
+print(f"URL: {url}")
 
-# choice = ("Video", "best[ext=mp4]", "mp4")
 
-# _, format, ext = choice
-# print("format: %s" % (format))
 
 opts = {
     "outtmpl": os.path.join(outdir, "%(title)s.%(ext)s"),
-    "format": "bestaudio/best",
     "nocheckcertificate": True,
-    "logger": MyLogger()
-    # "output": "%(title)s.%(ext)s"
+    "restrictfilenames": True
 }
 
 with YoutubeDL(opts) as ydl:
     info = ydl.extract_info(url, download=True)
     filepath = ydl.prepare_filename(info)
 
-print(f"Downloaded: {filepath}")
+print(f"Downloaded: {info['title']}.{info['ext']}")
 
 try:
+    console.quicklook(filepath)
     console.open_in(filepath)
 finally:
     os.remove(filepath)
-    print(f"Deleted: {filepath}")
+    print(f"Deleted: {info['title']}.{info['ext']}")
